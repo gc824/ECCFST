16b4e1012e91ab3aa431f86b4e18111c2154ee18
Gagan Chhabra <gchhabra24@gmail.com>
2021-02-13 15:28:22 -0500

    made updates

--- Homework/Madlibs.php
+++ Homework/Madlibs.php
@@ -1,32 +1,51 @@
 <!doctype html>
 <html>
+
 <head>
-<title>Madlibs</title>
+    <title>Madlibs</title>
 </head>
+
 <body>
 
-<h1>Madlibs</h1>
+    <h1>Madlibs</h1>
 
-<div id="madlibs" name="madlibs">
- <form method="post" name="madlibs">
-color: <input type="text" name="color" id="color"/> <br>
-noun: <input type="text" name="noun" id="noun"/><br>
-athlete Name: <input  type="text" name="athlete_name" id="athlete_name"/><br>
-<input type="submit" name="submit-btn" value="Libit!"/>
-</form>
-</div>
+    <div id="madlibs" name="madlibs">
+        <form method="post" name="madlibs">
+            Color: <input type="text" name="color" id="color" /> </br>
+            Noun: <input type="text" name="noun" id="noun" /><br>
+            Athlete Name: <input type="text" name="athleteName" id="athleteName" /></br>
+            <input type="submit" name="submit_btn" value="Libit!" />
+        </form>
+        </br>
+    </div>
 
-<?php
+    <?php
 
-$Color =$_POST["color"];
-$Noun =$_POST["noun"];
-$Athletename =$_POST["athlete_name"];
+    if (isset($_POST['submit_btn'])) {
 
-echo "gatorade are $Color";
-echo "yellow are $noun";
-echo "name of the athlete $atheletename" 
-
-?>
-
+        $color = $_POST["color"];
+        $noun = $_POST["noun"];
+        $athleteName = $_POST["athleteName"];
+        if (!empty($_POST['color'])) {
+            $color = $_POST['color'];
+        } else {
+            echo "<h3 style='color: red'> Please choose a color</h3>";
+        }
+        if (!empty($_POST['noun'])) {
+            $noun = $_POST['noun'];
+        } else {
+            echo "<h3 style='noun: red'> Please choose a noun</h3>";
+        }
+        if (!empty($_POST['athleteName'])) {
+            $athleteName = $_POST['athleteName'];
+        } else {
+            echo "<h3 style='color: red'> Please choose a athleteName</h3>";
+        }
+        echo "gatorade are $color";
+        echo "yellow are $noun";
+        echo "name of the athlete $athleteName";
+    }
+    ?>
 </body>
+
 </html>
