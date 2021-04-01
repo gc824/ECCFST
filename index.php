aacc781baffa2753074078a18078e5e66f4a922b
Gagan Chhabra <gchhabra24@gmail.com>
2021-04-01 17:50:19 -0400

    updated index file

--- index.php
+++ index.php
@@ -1,17 +1,31 @@
 <?php
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
-echo  __DIR__ . '/vendor/autoload.php';
-
 require __DIR__ . '/vendor/autoload.php';
 
-$app = AppFactory::create();
+//var_dump($_SERVER);
 
+$app = AppFactory::create();
+/**
+ * Add Error Handling Middleware
+ *
+ * @param bool $displayErrorDetails -> Should be set to false in production
+ * @param bool $logErrors -> Parameter is passed to the default ErrorHandler
+ * @param bool $logErrorDetails -> Display error details in error log
+ * which can be replaced by a callable of your choice.
+ 
+ * Note: This middleware should be added last. It will not handle any exceptions/errors
+ * for middleware added after it.
+ */
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+// define $app route->setBasePath('place you path name');
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
+	$response->getBody()->write("Hello world!");
+	return $response;
 });
 
 $app->run();
